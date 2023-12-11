e6f7780ac55489a137fc2dfc1986289aa84159ac
Dennis Hackethal <dh@dennishackethal.com>
2023-12-10 22:39:20 -0800

    Control truck forward/backward and sideways

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float turnSpeed;
+    public float forwardInput;
+    public float horizontalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +18,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move the vehicle forward. Seems like this could be done
+        // in just one method call.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
