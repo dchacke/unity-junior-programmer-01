4bdc930fcdcbec9c375b4128bb20170ed8ac3cfb
Dennis Hackethal <dh@dennishackethal.com>
2023-12-11 12:42:38 -0800

    Revert "Implement alternative camera view and toggle between cameras"
    This reverts commit 807437d1068b7f3a421e9624411ff0fbb3c6c539. Reverting this so I can work on a split-screen view instead.

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset;
+    private Vector3 offset = new Vector3(0, 5, -7);
 
     // Start is called before the first frame update
     void Start()
