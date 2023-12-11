f39abe86db46fb7638ec26310d65d89b9c9cd735
Dennis Hackethal <dh@dennishackethal.com>
2023-12-10 22:16:13 -0800

    Make camera follow truck

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Couldn't we just group the camera with the truck?
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
