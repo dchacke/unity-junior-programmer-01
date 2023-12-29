c5559ec0f95d656068c000c3a156c2ee8c8ec4b6
Dennis Hackethal <dh@dennishackethal.com>
2023-12-28 17:39:58 -0700

    Add RPM display

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float horsePower = 20.0f;
     [SerializeField] float turnSpeed = 45.0f;
     [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
     private float forwardInput;
     private float horizontalInput;
     private Rigidbody rb;
@@ -35,5 +36,8 @@ public class PlayerController : MonoBehaviour
     {
         float speed = Mathf.Round(rb.velocity.magnitude * 2.237f);
         speedometerText.SetText("Speed: " + speed + "mph");
+
+        float rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.SetText("RPM: " + rpm);
     }
 }
