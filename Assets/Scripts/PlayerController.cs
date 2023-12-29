09d2004d0170391d364ef869d68d1c3730dc00ae
Dennis Hackethal <dh@dennishackethal.com>
2023-12-28 17:33:40 -0700

    Add speedometer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float horsePower = 20.0f;
     [SerializeField] float turnSpeed = 45.0f;
+    [SerializeField] TextMeshProUGUI speedometerText;
     private float forwardInput;
     private float horizontalInput;
     private Rigidbody rb;
@@ -27,5 +29,11 @@ public class PlayerController : MonoBehaviour
 
         // Rotate the vehicle based on horizontal input
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+
+    void Update()
+    {
+        float speed = Mathf.Round(rb.velocity.magnitude * 2.237f);
+        speedometerText.SetText("Speed: " + speed + "mph");
     }
 }
